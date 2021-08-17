a3aefb2239c33f01d93f4ab28e0fb8e73d88b7af
cmskzhan <kaizhang@hotmail.com>
2021-08-17 11:46:01 +0100

    Helloworld2 (#1)
    * Snapshot 'helloworld'
    
    * formatted
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * Autosave
    
    * flutter part2 widgets
    
    * Autosave
    
    * part 2 scaffold
    
    Co-authored-by: FlutLab.io <>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text('Hello World 2!'))));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.amberAccent,
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.redAccent,
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://media-exp1.licdn.com/dms/image/C4D03AQEdHrby2IzlZg/profile-displayphoto-shrink_400_400/0/1516542582287?e=1634774400&v=beta&t=6greFQvped7cSa0ayFxSxPuAOKAmqV5xhfrdnKYOHI4'),
+            ),
+          ),
+        ),
+      ),
+    );
