6b42db50745cb67bbbfd29ba86ff641c16ffe392
cmskzhan <kaizhang@hotmail.com>
2021-08-20 12:42:40 +0100

    a helloworld copy from https://flutter.dev/docs/development/ui/widgets-intro

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.amberAccent,
-          appBar: AppBar(
-            title: Text('I Am Rich'),
-            backgroundColor: Colors.redAccent,
-          ),
-          body: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://i0.wp.com/flutteragency.com/wp-content/uploads/2020/10/iOS-Emulator.png"),
-                fit: BoxFit.cover
-              ),
-            ),
-            child: Center(
-              child: Image(
-                image: AssetImage('images/CaptureBBC.JPG'),
-              ),
-            ),
-          ),
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
+
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for
+    // the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
         ),
+        title: const Text('Example title'),
+        actions: const [
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      // body is the majority of the screen.
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
       ),
     );
+  }
+}
