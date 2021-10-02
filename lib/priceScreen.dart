c482befe6115902d202e28aeff2b197ff32ebe67
cmskzhan <kaizhang@hotmail.com>
2021-10-02 22:42:46 +0100

    List<DropdownMenuItem<String>> getDropdownItems()

--- lib/priceScreen.dart
+++ lib/priceScreen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'coinData.dart';
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({ Key? key }) : super(key: key);
@@ -9,11 +9,32 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+  String selectedValue = currenciesList[5];
+
+  void getCurrenciesList () {
+    for ( var i in currenciesList ){
+      print(i);
+    }
+  }
+
+  List<DropdownMenuItem<String>> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (var i in currenciesList) {
+      DropdownMenuItem<String> newItem = DropdownMenuItem(child: Text(i), value: i);
+      dropdownItems.add(newItem);
+    }
+    return dropdownItems;
+  }
+
   @override
   Widget build(BuildContext context) {
+    getCurrenciesList();
     return Scaffold(
       appBar: AppBar(title: Text('Crypto Coins Ticker'),),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
           child: Card(
@@ -24,7 +45,22 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Text('1BTC = ? USD', style: TextStyle(fontSize: 20, color: Colors.white), textAlign: TextAlign.center,),),
             ),),
           
-          Container(height: 150, alignment: Alignment.center, color: Colors.lightBlue, child: null,)
+          Container(
+            height: 150,
+            alignment: Alignment.center,
+            color: Colors.lightBlue,
+            child: DropdownButton<String>(
+              value: selectedValue, // has to be the value below
+              items: getDropdownItems(),
+              onChanged: (dropDownSelected) {
+                setState(() {
+                  selectedValue = dropDownSelected.toString();
+                  print(selectedValue);
+                });
+
+              },
+            ),
+          )
         ],
       ),
     );
