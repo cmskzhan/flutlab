d411349bff9f93db0c481f532d2937ec9f6f9886
cmskzhan <kaizhang@hotmail.com>
2021-10-25 11:48:13 +0100

    gtAlTodo().then((value)=>print(value)); shrinkWrap

--- lib/input.dart
+++ lib/input.dart
@@ -23,36 +23,45 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Left swipe ListView Future Demo"),),
-      body: FutureBuilder(
-        builder: (context, AsyncSnapshot snapshot) {
-          // WHILE THE CALL IS BEING MADE AKA LOADING
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          // WHEN THE CALL IS DONE BUT HAPPENS TO HAVE AN ERROR
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-
-
-          // IF URL WORKS IT GOES HERE!
-          return ListView.separated(
-            separatorBuilder: (BuildContext context, int index) => const Divider(),
-            itemCount: snapshot.data.length,
-            itemBuilder: (context, index) {
-              final item = snapshot.data[index].toString();
-              return Dismissible(
-                key: Key(item),
-                child: ListTile(
-                  title: Text(snapshot.data[index]['title']),
-                  subtitle: Text(snapshot.data[index].toString()),
-                ),
-              );
-            },);
-        },
-        future: getAllTodos(),
-        ),
+      body: Column(
+        children: [
+          TextButton(onPressed: (){getAllTodos().then((value) => print(value));}, child: Text("getAllTodos values in console")),
+          Expanded(
+            child: FutureBuilder(
+              builder: (context, AsyncSnapshot snapshot) {
+                // WHILE THE CALL IS BEING MADE AKA LOADING
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+          
+                // WHEN THE CALL IS DONE BUT HAPPENS TO HAVE AN ERROR
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+          
+          
+                // IF URL WORKS IT GOES HERE!
+                return ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  separatorBuilder: (BuildContext context, int index) => const Divider(),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    final item = snapshot.data[index].toString();
+                    return Dismissible(
+                      key: Key(item),
+                      child: ListTile(
+                        title: Text(snapshot.data[index]['title']),
+                        subtitle: Text(snapshot.data[index].toString()),
+                      ),
+                    );
+                  },);
+              },
+              future: getAllTodos(),
+              ),
+          ),
+        ],
+      ),
       
     );
   }
