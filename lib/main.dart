dc741faf388d8a477f26704fd2cec8ac49d1f280
cmskzhan <kaizhang@hotmail.com>
2021-11-04 12:19:01 +0000

    Pure example from github

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'same_variable_multiple_animations.dart';
+import 'sequence_page.dart';
+import 'staggered_animation_replication.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Text("hello world"),
-      ),
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '3rd party animation',
+      theme: ThemeData(primaryColor: Colors.blue),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => HomePage(),
+        '/sequence': (context) => SequencePage(),
+        '/StaggeredAnimationReplication': (context) => StaggeredAnimationReplication(),
+        '/SameVariableAnimationPage': (context) => SameVariableAnimationPage(),
+      },
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Animation Example"),),
+      body: Center(child: Column(
+        children: [
+          ElevatedButton(onPressed: () {Navigator.pushNamed(context, '/sequence');}, child: Text("Sequence")),
+          ElevatedButton(onPressed: () {Navigator.pushNamed(context, '/StaggeredAnimationReplication');}, child: Text("StaggeredAnimationReplication")),
+          ElevatedButton(onPressed: () {Navigator.pushNamed(context, '/SameVariableAnimationPage');}, child: Text("SameVariableAnimationPage")),
+        ],
+      )),
+    );
+  }
+}
